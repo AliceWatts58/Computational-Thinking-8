317401390ab0d1506e8024d3405d93131eb048ff
AliceWatts58 <alicewatts@seattleacademy.org>
2025-09-22 19:17:19 +0000

    Project 2

--- project2.py
+++ project2.py
@@ -2,4 +2,44 @@ book_points = 0
 movie_points = 0
 
 
-answer = input ("do you either like to ")
+answer = input ("do you have A) a short attention span, or B) have a long attention span?")
+if answer == "A":
+    movie_points += 1
+elif answer == "B":
+    book_points += 1
+
+
+answer = input ("are you a A) a visual learner, or B) a auditory learners?")
+if answer == "A":
+    movie_points += 1
+elif answer == "B":
+    book_points += 1
+
+
+answer = input ("do you like A) dark chocolate, or B) milk chocolate?")
+if answer == "A":
+    book_points += 1
+elif answer == "B":
+    movie_points += 1
+
+
+answer = input ("do you like A) primary color wheel or B) black and white color scale")
+if answer == "A":
+    movie_points += 1
+elif answer == "B":
+    book_points += 1
+
+
+answer = input ("are you energetic A) no or B) yes")
+if answer == "A":
+    book_points += 1
+elif answer == "B":
+    movie_points += 1
+
+print (f"movie points = {movie_points}")
+print (f"book_points = {book_points}")
+# end of quiz
+if book_points > movie_points:
+    print ("You are a book person")
+elif movie_points > book_points:
+    print (" You are a movie person")
