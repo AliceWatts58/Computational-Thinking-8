1d6e9fa9eb3aa25488f8811a05160e7a04ac46fe
AliceWatts58 <alicewatts@seattleacademy.org>
2025-10-08 21:52:54 +0000

    project 2 revisions

--- project2.py
+++ project2.py
@@ -24,14 +24,14 @@ elif answer == "B":
 
 
 answer = input ("do you like A) primary color wheel or B) black and white color scale")
-if answer == "A":
+if answer == "A": 
     movie_points += 1
 elif answer == "B":
     book_points += 1
 
 
 answer = input ("are you energetic A) no or B) yes")
-if answer == "A":
+if answer == "A" or answer == "a":
     book_points += 1
 elif answer == "B":
     movie_points += 1
@@ -43,3 +43,7 @@ if book_points > movie_points:
     print ("You are a book person")
 elif movie_points > book_points:
     print (" You are a movie person")
+if movie_points > book_points and movie_points > 3: 
+    print ("you really like movies dont you.")
+if book_points > movie_points and book_points > 3:
+    print ("wow why do you like books so much")
